286acc90807f3b40c270caeb1e926a854861ef41
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-02-03 15:59:12 +0600

    Null Vs Undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+// let name;
+
+function add(n1, n2) {
+    // console.log(n1 + n2);
+    // return n1 + n2;
+    return
+}
+// console.log(name);
+console.log(add(7));
+
+const name = {
+    name: "iamraufu",
+    from: "127.0.0.1"
+};
+console.log(name.to);
+
+let say = undefined;
+console.log(say)
+
+let num = [1, 2, 3];
+console.log(num[5]);
